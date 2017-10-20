62c40592c38de53bf36a8739a4680df2a3ccce51
Ryan Noort <ryannoort@hotmail.com>
2017-10-20 15:11:18 -0600

    Various changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "MrClean@mrclean.ca"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
 class UserMailer < ApplicationMailer
-  default from: "MrClean@mrclean.ca"
-
   def assignment_reminder(employee)
     @employee = employee
 
-    mail(to: 'smtp://localhost.1025', subject: "Test email")
+    mail(to: @employee.email, subject: "Kitchen Duties Reminder")
   end
 end
