5a4a85e38b6ac210f49a81db42aa2720d85fd91b
Ryan Noort <ryannoort@hotmail.com>
2017-10-23 15:47:04 -0600

    change enviorment variable names

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Setup the mailer config
-  config.action_mailer.default_url_options = {host: ENV['HOST']}
+  config.action_mailer.default_url_options = {host: ENV['MAILER_URL']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
